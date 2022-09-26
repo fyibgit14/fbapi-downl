b965f9782fc2c2db19f4ff8131e518acd2079bd1
fyibgit14 <94876704+fyibgit14@users.noreply.github.com>
2022-09-26 16:08:51 +0100

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 
 var app = express();
 const port = process.env.PORT || 3000;
+app.use(cors());
 app.use((req, res, next) => {
       res.header('Access-Control-Allow-Origin', '*');
       next();
